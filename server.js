9b78aa685a9b71a7aca5b11446c9386cca4011d7
unknown <Poramat Nukliang>
2018-09-17 10:48:42 +0700

    finish1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var app = express();
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -14,6 +15,13 @@ app.get('/', function(req, res) {
 
 
 app.get('/students', function(req, res) {
+    var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
   connection.connect()
 connection.query('SELECT * from students', function (err, rows, fields) {
 
@@ -27,6 +35,13 @@ connection.query('SELECT * from students', function (err, rows, fields) {
 });
 
 app.get('/subjects', function(req, res) {
+    var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
   connection.connect()
 connection.query('SELECT * from subjects', function (err, rows, fields) {
 
